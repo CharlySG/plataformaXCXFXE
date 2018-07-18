f02e862967bb8c80909bb7e4c8d2e7f3a047fc64
Carlos Sarmiento <caifanes_Carlos11@hotmail.com>
2018-07-18 15:36:34 -0500

    sidebar menu configurated

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -7,22 +7,56 @@
       <a href="#name"><span class="white-text name">{{Auth::user()->name}}</span></a>
       <a href="#email"><span class="white-text email">{{Auth::user()->email}}</span></a>
     </div></li>
-    <li><a href="#!">First Link With Icon</a></li>
-    <li><a href="#!">Second Link</a></li>
+    {{-- <li><a href="#!">First Link With Icon</a></li>
+    <li><a href="#!">Second Link</a></li> --}}
     <li><div class="divider"></div></li>
+    <li><a href="#!">SAIDI POR CAUSA</a></li>
+    <li><a href="#!">CONFIABILIDAD MENSUAL</a></li>
+    <li><a href="#!">SAIDI POR CIRCUITO VS MANTTO</a></li>
     <li class="no-padding">
         <ul class="collapsible collapsible-accordion">
-          <li class="bold"><a class="collapsible-header waves-effect waves-teal" tabindex="0"><i class="material-icons">assessment</i>JavaScript</a>
+          <li class="bold">
+            <a class="collapsible-header waves-effect waves-teal" tabindex="0"><i class="material-icons">assessment</i>RESULTADO META VS REAL</a>
               <div class="collapsible-body" style="">
                 <ul>
-                  <li><a href="auto-init.html">Auto Init</a></li>
-                  <li><a href="carousel.html">Carousel</a></li>
-                  <li class="active"><a href="collapsible.html">Collapsible</a></li>
-                  <li><a href="dropdown.html">Dropdown</a></li>
-                  <li><a href="feature-discovery.html">FeatureDiscovery</a></li>
+                  <li><a href="auto-init.html">SAIDI</a></li>
+                  <li><a href="carousel.html">SAIFI</a></li>
+                  <li class="active"><a href="collapsible.html">CAIDI</a></li>
+                  <li><a href="dropdown.html">TPR</a></li>
+                  <li><a href="feature-discovery.html">NI</a></li>
+                  <li><a href="feature-discovery.html">UPA</a></li>
                 </ul>
               </div>
             </li>
+            <li class="bold">
+              <a class="collapsible-header waves-effect waves-teal" tabindex="0"><i class="material-icons">assessment</i>RAMALES POR CAUSA</a>
+                <div class="collapsible-body" style="">
+                  <ul>
+                    <li><a href="auto-init.html">LMX</a></li>
+                    <li><a href="carousel.html">GIA</a></li>
+                    <li class="active"><a href="collapsible.html">TGD</a></li>
+                    <li><a href="dropdown.html">JUY</a></li>
+                    <li><a href="feature-discovery.html">VFD</a></li>
+                    <li><a href="auto-init.html">RDB</a></li>
+                    <li><a href="auto-init.html">TGU</a></li>
+                    <li><a href="auto-init.html">TXN</a></li>
+                    <li><a href="auto-init.html">TXS</a></li>
+                    <li><a href="auto-init.html">OCZ</a></li>
+                    <li><a href="auto-init.html">IPD</a></li>
+                    <li><a href="auto-init.html">LGZ</a></li>
+                    <li><a href="auto-init.html">CIT</a></li>
+                    <li><a href="auto-init.html">MAA</a></li>
+                    <li><a href="auto-init.html">COP</a></li>
+                    <li><a href="auto-init.html">MPE</a></li>
+                    <li><a href="auto-init.html">PED</a></li>
+                    <li><a href="auto-init.html">SMJ</a></li>
+                    <li><a href="auto-init.html">AAA</a></li>
+                    <li><a href="auto-init.html">SOY</a></li>
+                    <li><a href="auto-init.html">PAR</a></li>
+                    <li><a href="auto-init.html">BBN</a></li>
+                  </ul>
+                </div>
+              </li>
         </ul>
       </li>
   </ul>
