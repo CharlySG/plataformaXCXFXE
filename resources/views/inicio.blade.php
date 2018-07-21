1b5ef9b4e2437cb515a2d85fead1ebec3e13cb91
Jhovany Morales Bruno <jhovanymb.info@gmail.com>
2018-07-20 19:51:10 -0500

    decimals reduction

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.principalapp')
 @section('content')
-<div class="container">
-  <div id="renderme">
-    <div class="center" style="color:green">
-      <h1 style="margin-top:200px;">Reportes de Confiabilidad</h1>
-    </div>
+<div id="renderme">
+  <div class="center" style="color:green">
+    <h1 style="margin-top:200px;">Reportes de Confiabilidad</h1>
   </div>
 </div>
 @endsection
